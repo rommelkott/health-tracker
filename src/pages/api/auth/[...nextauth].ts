d4a1e9817e0998bd3877d3bb1eae611e186162d1
Rommel Kott <esteban.kott@gmail.com>
2022-12-26 13:07:33 -0600

    Create profile for new users

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -5,6 +5,7 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
 import { env } from "../../../env/server.mjs";
 import { prisma } from "../../../server/db/client";
+import { userAgent } from "next/server.js";
 
 export const authOptions: NextAuthOptions = {
   // Include user.id on session
@@ -13,9 +14,35 @@ export const authOptions: NextAuthOptions = {
       if (session.user) {
         session.user.id = user.id;
       }
+
       return session;
     },
   },
+  events: {
+    async signIn({ isNewUser, user }) {
+      // Default user profile for new users
+      const userProfile = {
+        description: "Hi, I'm a new user.",
+      };
+
+      // If the user is new, update the user's profile
+      if (isNewUser) {
+        const addProfile = await prisma.user.update({
+          where: {
+            id: user.id,
+          },
+          data: {
+            profile: {
+              create: {
+                ...userProfile,
+              }
+            }
+          }
+        });
+      }
+    },
+  },
+
   // Configure one or more authentication providers
   adapter: PrismaAdapter(prisma),
   providers: [
