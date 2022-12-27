d0a474ad2f1dd26d97424ccb14efc945c103d0ec
Rommel Kott <esteban.kott@gmail.com>
2022-12-27 16:21:52 -0600

    Add header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Session } from "next-auth";
+import { ProfileButton } from "./ProfileButton";
+
+interface HeaderProps {
+    status: "authenticated" | "loading" | "unauthenticated";
+    session: Session | null;
+    handleSignIn?: () => void;
+    handleSignOut?: () => void;
+}
+
+export const Header: React.FC<HeaderProps> = (
+    {
+        status,
+        session,
+        handleSignIn,
+        handleSignOut,
+    }
+) => {
+    return (
+        <div className="flex flex-col">
+            <div className="flex flex-row justify-between items-center bg-gray-900 p-4">
+                <a href="/" className="text-gray-200 font-bold text-2xl">Health Tracker</a>
+                <ProfileButton
+                    session={session}
+                    status={status}
+                    handleSignIn={handleSignIn}
+                    handleSignOut={handleSignOut}
+                />
+            </div>
+        </div>
+    );
+};
+
+export default Header;
