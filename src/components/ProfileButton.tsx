d1691317fd72ad794598bfbb80e5c2079fd49282
Rommel Kott <esteban.kott@gmail.com>
2022-12-27 12:04:30 -0600

    Add dropdown menu to profile

--- src/components/ProfileButton.tsx
+++ src/components/ProfileButton.tsx
@@ -1,5 +1,6 @@
 import { Session } from "next-auth";
 import React, { Component } from "react";
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
 
 interface ProfileButtonProps {
   status: "authenticated" | "loading" | "unauthenticated";
@@ -19,17 +20,42 @@ export const ProfileButton: React.FC<ProfileButtonProps> = ({
       <div className="flex items-center gap-2">
         {status === "authenticated" && session && (
           <>
-            <div className="flex items-center gap-2">
-              <img
-                className="h-10 w-10 rounded-full"
-                src={session.user?.image || ""}
-                alt="profile"
-                onClick={handleSignOut}
-              />
-              {/* <span className="text-lg font-bold text-black">
-                {session.user?.name || "User"}
-              </span> */}
-            </div>
+            <DropdownMenu.Root>
+              <DropdownMenu.Trigger className="flex items-center gap-2">
+                <img
+                  className="h-10 w-10 rounded-full select-none"
+                  src={session.user?.image || ""}
+                  alt="profile"
+                />
+              </DropdownMenu.Trigger>
+              <DropdownMenu.Content
+                sideOffset={5}
+                className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+                align="end"
+              >
+                <div className="py-1">
+                  <DropdownMenu.Item
+                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                    onSelect={() => (window.location.href = "/u/" + session.user?.id)}
+                  >
+                    Your Profile
+                  </DropdownMenu.Item>
+                  <DropdownMenu.Item
+                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                    onSelect={() => (window.location.href = "/settings")}
+                  >
+                    Account Settings
+                  </DropdownMenu.Item>
+                  <DropdownMenu.Item
+
+                    className="block px-4 py-2 text-sm text-red-700 hover:bg-red-100"
+                    onSelect={handleSignOut}
+                  >
+                    Sign out
+                  </DropdownMenu.Item>
+                </div>
+              </DropdownMenu.Content>
+            </DropdownMenu.Root>
           </>
         )}
         {status === "unauthenticated" && (
