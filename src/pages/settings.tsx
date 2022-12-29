05babbb8e7da7f8cf1dec7ce8f8ad3bcd0e4eb8b
Rommel Kott <esteban.kott@gmail.com>
2022-12-28 21:48:06 -0600

    Ensure text wrapped in account settings

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -53,15 +53,15 @@ const Home: NextPage = () => {
           <div className="container flex max-w-4xl flex-col justify-center gap-4 px-4 py-6">
             <div className="inline-flex items-center justify-between">
               <div>
-                <h1 className="mb-2 text-5xl font-bold text-black">Settings</h1>
-                <h2>Adjust your account and profile</h2>
+                <h1 className="mb-1 text-5xl font-bold text-black">Settings</h1>
+                <h2>Adjust your account</h2>
               </div>
             </div>
             <InformationCard
               IconComponent={HiInformationCircle}
-              title="This page is under development"
+              title="Page is under development"
               titleColor="text-orange-600"
-              bodyText="We are currently working on this page. In the meantime, you can use our API to update your profile. To obtain an API key, click the button below."
+              bodyText="We are currently working on this page. In the meantime, you can use our API to update your profile."
             />
 
             <Card>
@@ -69,13 +69,13 @@ const Home: NextPage = () => {
               <div className="flex flex-col gap-4">
                 <div className="flex flex-col gap-4">
                   <h3 className="text-lg">This is your unique API key, which will only be shown to you once after it is generated. Keep it private and secure to protect your account</h3>
-                  <div className="flex flex-row gap-3 items-center mt-3">
+                  <div className=" gap-3 items-center mt-3">
                     <span className="text-lg font-semibold">Key</span>
-                    <span className="select-all bg-gray-100 text-black font-mono font-bold py-2 px-2 rounded w-96"> {APIKey.data || 'my-example-key'} </span>
+                    <p className="select-all bg-gray-100 text-black font-mono font-bold py-2 px-2 rounded break-all"> {APIKey.data || 'my-example-key'} </p>
                   </div>
-                  <div className="flex flex-row gap-3">
+                  <div className="flex flex-row flex-wrap gap-3">
                     <button onClick={() => APIKey.mutate()} className="bg-blue-600 text-white font-bold py-2 px-4 rounded max-w-md:">Generate API Key</button>
-                    <a href="/documentation" className=" border border-blue-600 text-blue-600 font-bold py-2 px-4 rounded max-w-md:">Developer Documentation</a>
+                    <a href="/documentation" className=" border border-blue-600 text-blue-600 font-bold py-2 px-4 rounded max-w-md:">Documentation</a>
                   </div>
                 </div>
               </div>
